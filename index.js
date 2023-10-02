6b314a79f8ed2311c72d66a12bc381b981c590d6
Swapnil <swapnilpingale22@gmail.com>
2023-10-02 11:26:21 +0530

    dotenv added

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+
 const express = require("express");
 
 const urlRoute = require("./routes/url");
@@ -8,9 +10,13 @@ const { connectToMongoDB } = require("./connect")
 
 const app = express();
 
-const PORT = 8001;
+const port = process.env.PORT;
 
-connectToMongoDB("mongodb://127.0.0.1:27017/short-url")
+//local DB
+// connectToMongoDB("mongodb://127.0.0.1:27017/short-url")
+//     .then(() => console.log('MongoDB connected'));
+
+connectToMongoDB("mongodb+srv://swapnil:cws123@cluster0.ys61ljb.mongodb.net/?retryWrites=true&w=majority")
     .then(() => console.log('MongoDB connected'));
 
 app.use(express.json());
@@ -20,6 +26,6 @@ app.use("/", redirectRoute);
 
 
 
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
 });
