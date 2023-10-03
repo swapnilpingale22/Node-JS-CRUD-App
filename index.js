c420113eb91b5ab3ee5f74c4872938518aa64f86
Swapnil <swapnilpingale22@gmail.com>
2023-10-03 11:14:02 +0530

    UI fixes

--- index.js
+++ index.js
@@ -4,20 +4,19 @@ const express = require("express");
 
 const path = require("path");
 
-const urlRoute = require("./routes/url");
+const app = express();
 
 const staticRoute = require("./routes/staticRouter");
-
 const redirectRoute = require("./routes/shortid");
+const { connectToMongoDB } = require("./connect");
+const urlRoute = require("./routes/url");
 
-const { connectToMongoDB } = require("./connect")
 
-const URL = require('./models/url')
-
-const app = express();
 
 // Set EJS as the view engine
 app.set('view engine', 'ejs');
+
+// set path of views
 app.set("views", path.resolve("./views"));
 
 const port = process.env.PORT;
@@ -29,32 +28,15 @@ const port = process.env.PORT;
 connectToMongoDB("mongodb+srv://swapnil:cws123@cluster0.ys61ljb.mongodb.net/?retryWrites=true&w=majority")
     .then(() => console.log('MongoDB connected'));
 
+//middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.get("/test/allurl", async (req, res) => {
-
-    const allUrls = await URL.find({});
-    return res.render("home", { urls: allUrls, });
-});
-
 app.use("/url", urlRoute);
 app.use("/id", redirectRoute);
-
 app.use("/", staticRoute);
-
-
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
 
-
-
-{/* <div class="container">
-<% urls.forEach(url=> { %>
-    <li>
-        <%= url.shortId %> - <%= url.redirectUrl %> - <%= url.visitHistory.length %>
-    </li>
-    <% }) %>
-</div> */}
